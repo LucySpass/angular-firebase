0e58903cb47145d59fa87cc187ced2e020ad9129
ivana.begovic@yahoo.com <ivana.begovic@yahoo.com>
2018-06-29 00:55:21 +0300

    [add]: fixed materialize and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,15 @@ import {AngularFirestore} from 'angularfire2/firestore';
 import {Observable} from 'rxjs';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'dog park';
-    items: Observable<any[]>;
+  title = 'dog park';
+  items: Observable<any[]>;
 
-    constructor(private db: AngularFirestore) {
-        this.items = db.collection('dogs').valueChanges();
-    }
+  constructor(private db: AngularFirestore) {
+    this.items = db.collection('dogs').valueChanges();
+  }
 }
