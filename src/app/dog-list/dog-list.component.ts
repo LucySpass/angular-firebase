6633d904357a039284d3744ccea593b25683e41a
ivana.begovic@yahoo.com <ivana.begovic@yahoo.com>
2018-06-28 23:07:29 +0300

    [fix]: readme.md

--- src/app/dog-list/dog-list.component.ts
+++ src/app/dog-list/dog-list.component.ts
@@ -3,18 +3,18 @@ import {AngularFirestore} from 'angularfire2/firestore';
 import {Observable} from 'rxjs/index';
 
 @Component({
-    selector: 'app-dog-list',
-    templateUrl: './dog-list.component.html',
-    styleUrls: ['./dog-list.component.css']
+  selector: 'app-dog-list',
+  templateUrl: './dog-list.component.html',
+  styleUrls: ['./dog-list.component.css']
 })
 export class DogListComponent implements OnInit {
-    items: Observable<any[]>;
+  items: Observable<any[]>;
 
-    constructor(private db: AngularFirestore) {
-        this.items = db.collection('dogs').valueChanges();
-    }
+  constructor(private db: AngularFirestore) {
+    this.items = db.collection('dogs').valueChanges();
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
 
 }
